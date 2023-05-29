53aba610a3d005a33249b3c97db7a8e033907b04
Wu Zhenyu <wuzhenyu@ustc.edu>
2023-05-29 17:25:18 +0800

    :green_heart: Use github-push-action

--- scripts/git-commit.sh
+++ scripts/git-commit.sh
@@ -12,6 +12,3 @@ git config --global user.name 'Github Actions'
 git config --global user.email '41898282+github-actions[bot]@users.noreply.github.com'
 git commit -m ":bookmark: Dump version to $version"
 git tag "$version"
-git remote set-url origin "https://x-access-token:$GH_TOKEN@github.com/$GITHUB_REPOSITORY"
-git push
-git push --tags
