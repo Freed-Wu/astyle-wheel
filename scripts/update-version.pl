c960c52d1330b7bc4cad0ed2aadeb7a0e3812a7c
Wu, Zhenyu <wuzhenyu@ustc.edu>
2024-12-30 16:31:32 +0800

    :construction_worker: Use git-auto-commit-action

---
+++ scripts/update-version.pl
@@ -0,0 +1,11 @@
+#!/usr/bin/env -S perl -pi
+BEGIN {
+    exit if scalar @ARGV == 0;
+    $version =
+`curl 'https://gitlab.com/api/v4/projects/41218592/repository/tags?per_page=1' | jq -r '.[].name'`;
+}
+
+s/(?<=^    rev: )\S+/$version/;
+s/(?<=^set\(VERSION )\S+/$version/;
+s/(?<=astyle==)[^"]+/$version/;
+s/(?<=version = ")[^"]+/$version/;
