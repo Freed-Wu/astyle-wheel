ab31e4ef603ca40f9ffefe963444a927a0c2122d
Wu, Zhenyu <wuzhenyu@ustc.edu>
2024-12-31 14:14:21 +0800

    :ambulance: Chomp $version

--- scripts/update-version.pl
+++ scripts/update-version.pl
@@ -3,6 +3,7 @@ BEGIN {
     exit if scalar @ARGV == 0;
     $version =
 `curl 'https://gitlab.com/api/v4/projects/41218592/repository/tags?per_page=1' | jq -r '.[].name'`;
+    chomp $version;
 }
 
 s/(?<=^    rev: )\S+/$version/;
